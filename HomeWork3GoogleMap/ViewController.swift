e0632b70930e707bbb307b127c9dc7ea45b9b543
Valeriia Zakharova <zakh.valeriia@gmail.com>
2019-12-27 14:38:06 +0200

    initial commit

---
+++ HomeWork3GoogleMap/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  HomeWork3GoogleMap
+//
+//  Created by Valeriia Zakharova on 27.12.2019.
+//  Copyright © 2019 Valeriia Zakharova. All rights reserved.
+//
+
+import UIKit
+import GooglePlaces
+import GoogleMaps
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
